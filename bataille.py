0e1b3029ae7dac7b91f2e56e697508eb5b2649ea
fkdy01 <fdieny@free.fr>
2020-04-16 14:08:28 +0200

    refactoring

--- bataille.py
+++ bataille.py
@@ -1,5 +1,5 @@
 #evol1
-from random import randint,seed
+from random import randint,seed,shuffle
 lcouleurs = ["ROUGE","NOIR"] 
 lvaleurs =[ (0,"2"),
             (1,"3"),
@@ -27,10 +27,10 @@ class Carte:
 
     
     def affiche(self):
-        print(self.valeur+"(" + str(self.valeurnum) + ") de " + self.enseigne + " de couleur " + self.couleur ) 
+        print(self.valeur+"(" + str(self.valeurnum) + ") de " + self.enseigne ) 
 
     def __str__(self):
-        return self.valeur+"(" + str(self.valeurnum) + ") de " + self.enseigne + " de couleur " + self.couleur
+        return self.valeur+"(" + str(self.valeurnum) + ") de " + self.enseigne
 
 class Paquet:
     def __init__(self,type):
@@ -54,13 +54,30 @@ class Paquet:
         self.cartes[index2]=carte
 
     def melange(self):
-        for i in range(500):
-            i1=randint(0,len(self.cartes)-1)
-            i2=randint(0,len(self.cartes)-1)
-            self.switch(i1,i2)
+        shuffle(self.cartes)
+
+    def prend_carte(self):
+        return 
+    
+    def distribue(self,p1,p2):
+        # tour=1
+        # for carte in self.cartes:
+        #     if tour==1 :
+        #         p1.ajoute_carte(self.cartes.pop())
+        #         tour=2
+        #     else :
+        #         p2.ajoute_carte(self.cartes.pop())
+        #         tour=1
+        # print()
+ 
+        p1.cartes = self.cartes[1::2] 
+        p2.cartes = self.cartes[0::2]
+        print()
         
 seed()
 paquet=Paquet("PLEIN")
+paquetj1=Paquet(None)
+paquetj2=Paquet(None)
 paquet.melange()
-paquet.affiche()
+paquet.distribue(paquetj1,paquetj2)
 
